c91bbcaa6eb938d12202c2804502c569580c6341
Joni Juup <joni.juup@silo.ai>
2020-03-18 11:18:45 +0200

    changed the starter completely. now we are working...

--- data/config.js
+++ data/config.js
@@ -1,32 +1,32 @@
 module.exports = {
   // Name of your site. Can be the name of a brand, or your personal name.
-  title: 'Gatsby Starter',
+  title: 'Remotelooks',
   // What's the purpose of this website? What can we expect from it?
   description:
-    `A portfolio based on your latest Instagram posts, implemented with Uber's Base Web Design System.`,
+    `The hot and comfortable looks of people working remotely.`,
   // Your legal name.
-  legalName: 'Gatsby Starter',
+  legalName: 'Remotelooks',
   // URL to this website. If you bought `www.domain.com`, then it will be `www.domain.com`.
-  url: 'https://www.robonomy.com',
+  url: 'https://www.remotelooks.com',
   // Favicon that will display on browsers.
   logo: 'static/images/favicon.png',
   // Who made this website?
-  author: 'Juan Alejandro Morais',
+  author: 'Remotelooks team',
   // What's the website of the author?
-  authorUrl: 'https://www.timrodz.com',
+  authorUrl: 'https://www.remotelooks.com',
   // Your social media presence, in the form of usernames.
   social: {
     // tells the Instagram API the username to get posts from. 
     // If the url is `https://www.instagram.com/natgeotravel`, your username is `natgeotravel`.
-    instagram: 'natgeotravel', 
+    instagram: 'remotelooks', 
     // Optional
-    twitter: '@NatGeoTravel',
+    twitter: '@remotelooks',
   },
   socialLinks: {
     // Complete URL verison of `social.instagram`.
-    instagram: 'https://www.instagram.com/natgeotravel',
+    instagram: 'https://www.instagram.com/remotelooks',
     // Optional
-    twitter: 'https://twitter.com/NatGeoTravel',
+    twitter: 'https://twitter.com/remotelooks',
   },
   // If applicable, your Google Analytics crawl ID.
   googleAnalyticsId: 'UA-XXXXXXXX-X',
@@ -41,7 +41,7 @@ module.exports = {
   // Basic contact details.
   contact: {
     // Email address to contact you.
-    email: 'timrodz@icloud.com',
+    email: 'remotelooks@gmail.com',
   },
   // When was this website created?
   foundingDate: '2020',
